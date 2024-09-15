33bf683b0c2a56faf2d1448214c38420d44535b3
Pankaj Raghav <p.raghav@samsung.com>
2024-09-15 14:36:38 +0200

    quentin: added hardware quirks
    sleep works better

--- flake.nix
+++ flake.nix
@@ -5,76 +5,91 @@
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     agenix.url = "github:ryantm/agenix";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@ { nixpkgs, home-manager, agenix, ... }:
-  {
-    # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#your-hostname'
-    nixosConfigurations = {
+  outputs =
+    inputs@{
+      nixpkgs,
+      home-manager,
+      agenix,
+      nixos-hardware,
+      ...
+    }:
+    {
+      # NixOS configuration entrypoint
+      # Available through 'nixos-rebuild --flake .#your-hostname'
+      nixosConfigurations = {
 
-      blixen = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./blixen/configuration.nix
+        blixen = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./blixen/configuration.nix
 
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
 
-            home-manager.users.panky = import ./blixen/home.nix;
-          }
-        ];
+              home-manager.users.panky = import ./blixen/home.nix;
+            }
+          ];
+        };
+
+        trinity = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./trinity/configuration.nix
+            ./trinity/file.nix
+          ];
+        };
+
+        quentin = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            agenix.nixosModules.default
+            nixos-hardware.nixosModules.lenovo-thinkpad-x1-6th-gen
+            ./quentin/configuration.nix
+            ./quentin/age.nix
+            ./quentin/wireguard.nix
+            ./quentin/external-disk.nix
+            { environment.systemPackages = [ agenix.packages.x86_64-linux.default ]; }
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+
+              home-manager.users.panky = import ./quentin/home.nix;
+            }
+
+          ];
+        };
+
+        trinityiso = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            (
+              { pkgs, modulesPath, ... }:
+              {
+                imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
+                environment.systemPackages = [
+                  pkgs.vim
+                  pkgs.git
+                ];
+                users.users.nixos = {
+                  openssh.authorizedKeys.keys = [
+                    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKwykgce0ra9fqDt2fsi5f/5TDOjTL7LS2wiBXU6Y4Yl dev@pankajraghav.com"
+                  ];
+                };
+              }
+            )
+          ];
+        };
+
       };
-
-      trinity = nixpkgs.lib.nixosSystem {
-	system = "x86_64-linux";
-        modules = [./trinity/configuration.nix ./trinity/file.nix ];
-      };
-
-      quentin = nixpkgs.lib.nixosSystem {
-	system = "x86_64-linux";
-        modules = [
-	   agenix.nixosModules.default
-	  ./quentin/configuration.nix
-	  ./quentin/age.nix
-	  ./quentin/wireguard.nix
-	  ./quentin/external-disk.nix
-          {
-            environment.systemPackages = [ agenix.packages.x86_64-linux.default ];
-          }
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-
-            home-manager.users.panky = import ./quentin/home.nix;
-          }
-
-	];
-      };
-
-
-      trinityiso = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ({ pkgs, modulesPath, ... }: {
-            imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
-            environment.systemPackages = [ pkgs.vim pkgs.git];
-            users.users.nixos = {
-              openssh.authorizedKeys.keys = [
-                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKwykgce0ra9fqDt2fsi5f/5TDOjTL7LS2wiBXU6Y4Yl dev@pankajraghav.com"
-              ];
-            };
-          })
-        ];
-      };
-
     };
-  };
 }
