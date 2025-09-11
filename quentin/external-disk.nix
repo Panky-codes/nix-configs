267849858d41369c45b522a4bfe995522d2009f7
Pankaj Raghav <p.raghav@samsung.com>
2025-09-11 21:31:21 +0200

    quentin: change external backup disk

--- quentin/external-disk.nix
+++ quentin/external-disk.nix
@@ -1,16 +1,16 @@
 { config, pkgs, ... }:
 {
   environment.etc.crypttab.text = ''
-    vault UUID=1342cc60-7514-4d70-8d1b-303b009cea34 /root/mykeyfile.key noauto
+    backup UUID=03d495af-d106-44c2-a177-eed6e9471ad8 /root/mykeyfile.key noauto
   '';
 
   # The above crypttab creates a systemd cryptsetup vault service, which the below udev rule depends on
   services.udev.extraRules = ''
-    SUBSYSTEM=="block" ENV{ID_WWN}=="nvme.144d-533432304e50305234303030383659-53616d73756e6720506f727461626c6520535344205835-00000001", ENV{SYSTEMD_WANTS}="systemd-cryptsetup@vault.service"
+    SUBSYSTEM=="block" ENV{ID_SERIAL}=="Samsung_SSD_990_PRO_1TB_S6Z1NU0X263352V_1", ENV{SYSTEMD_WANTS}="systemd-cryptsetup@backup.service"
   '';
 
-  fileSystems."/mnt/vault" = {
-    device = "/dev/disk/by-uuid/e04b44ad-1beb-4902-9b91-e5e6ed43e51c";
+  fileSystems."/mnt/backup" = {
+    device = "/dev/disk/by-uuid/f82132a3-78f4-40d4-ba17-1bcc6c3d04c0";
     fsType = "btrfs";
     options = [
       "defaults"
@@ -23,24 +23,23 @@
 
   systemd.services.backupvault = {
     enable = true;
-    requires = [ "mnt-vault.mount" ];
-    after = [ "mnt-vault.mount" ];
-    wantedBy = [ "mnt-vault.mount" ];
+    requires = [ "mnt-backup.mount" ];
+    after = [ "mnt-backup.mount" ];
+    wantedBy = [ "mnt-backup.mount" ];
     serviceConfig = {
-      ExecStart = "${pkgs.systemd}/bin/systemctl start btrbk-vault.service";
+      ExecStart = "${pkgs.systemd}/bin/systemctl start btrbk-backup.service";
     };
   };
 
   services.btrbk = {
-    instances."vault" = {
-      onCalendar = "Mon..Fri, 15:30";
+    instances."backup" = {
       settings = {
         snapshot_preserve = "14d";
         snapshot_preserve_min = "2d";
         target_preserve = "7d 4w *m";
         stream_compress = "lz4";
         volume."/btr_pool" = {
-          target = "/mnt/vault/quentin-home";
+          target = "/mnt/backup/quentin-home";
           subvolume = "home";
           snapshot_create = "always";
         };
